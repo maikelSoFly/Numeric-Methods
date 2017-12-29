44da08e69de5f77d712df75dcdfd20e0c92016a1
maikelSoFly <mikolaj.stepniewski1@gmail.com>
2017-12-29 22:03:44 +0100

    printing with LaTeX syntax

--- NM--Euler-Method/NM--Euler-Method/main.cpp
+++ NM--Euler-Method/NM--Euler-Method/main.cpp
@@ -48,21 +48,29 @@ void PrintOutcome(double t, double x, double h, double i, functionPtr fun = NULL
 
 double Euler(double ts, double tf, double xs, double h, functionPtr f) {
     double t = ts, x = xs;
+    int interv = floor(((tf-ts)/h)/10);
     
-    for(int i = 0; t <= tf; ++i) {
-        PrintOutcome(t, x, h, i, f);
+    for(int i = 0; t <= tf+h; ++i) {
+        if (i % interv == 0) {
+            PrintOutcome(t, x, h, i, f);
+        }
         x += h * f(t, x);
         t += h;
     }
+    
+    
     
     return x;
 }
 
 double EulerHeun(double ts, double tf, double xs, double h, functionPtr f) {
     double t = ts, x = xs;
+    int interv = floor(((tf-ts)/h)/10);
     
-    for(int i = 0; t <= tf; ++i) {
-        PrintOutcome(t, x, h, i, f);
+    for(int i = 0; t <= tf+h; ++i) {
+        if (i % interv == 0) {
+            PrintOutcome(t, x, h, i, f);
+        }
         x += h * (f(t, x) + f(t+h, x + f(t, x) * h)) / 2;
         t += h;
     }
@@ -72,9 +80,12 @@ double EulerHeun(double ts, double tf, double xs, double h, functionPtr f) {
 
 double EulerMidPoint(double ts, double tf, double xs, double h, functionPtr f) {
     double t = ts, x = xs;
+    int interv = floor(((tf-ts)/h)/10);
     
-    for(int i = 0; t <= tf; ++i) {
-        PrintOutcome(t, x, h, i, f);
+    for(int i = 0; t <= tf+h; ++i) {
+        if (i % interv == 0) {
+            PrintOutcome(t, x, h, i, f);
+        }
         x += f(t + h/2, x + f(t, x) * h/2) * h;
         t += h;
     }
@@ -89,18 +100,19 @@ int main(int argc, const char * argv[]) {
     functionPtr f;
     
     // x' = t - x + 1       x(1) = 3,   1 < t < 2
-    ts = 1, tf = 2, xs = 3, h = 0.1, f = f1;
+    ts = 1, tf = 2, xs = 3, h = 0.01, f = f1;
+    
     
     cout << "PASTE THIS OUTPUT TO LATEX TABLE!" << endl << endl;
     cout << "Euler (podstawowy):" << endl << endl;
-    cout << "t_k & x_k & x(t_k) & \\epsilon \\\\ \\hline\n";
+    cout << "$t_k$ & $x_k$ & $x(t_k)$ & $\\epsilon$ \\\\ \\hline\n";
     Euler(ts, tf,xs, h, f);
     cout << endl << endl << "Euler (midpoint)" << endl << endl;
-    cout << "t_k & x_k & x(t_k) & \epsilon \\\\ \\hline\n";
+    cout << "$t_k$ & $x_k$ & $x(t_k)$ & $\\epsilon$ \\\\ \\hline\n";
     EulerMidPoint(ts, tf,xs, h, f);
     
     cout << endl << endl << "Euler (Heun):" << endl << endl;
-    cout << "t_k & x_k & x(t_k) & \epsilon \\\\ \\hline\n";
+    cout << "$t_k$ & $x_k$ & $x(t_k)$ & $\\epsilon$ \\\\ \\hline\n";
     EulerHeun(ts, tf, xs, h, f);
     cout << endl << endl;
 
